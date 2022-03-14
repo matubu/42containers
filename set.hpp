eb08313ab40719dd2c9f80b3b5ea6814a58758ae
Mathias Berger--fruhinsholz <mberger-@c1r4p3.42nice.fr>
2022-03-14 22:57:33 +0100

    moving to btree

--- set.hpp
+++ set.hpp
@@ -6,7 +6,7 @@
 /*   By: matubu <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 15:46:00 by matubu            #+#    #+#             */
-/*   Updated: 2022/02/09 15:50:38 by matubu           ###   ########.fr       */
+/*   Updated: 2022/03/14 22:46:35 by mberger-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@ namespace ft {
 		class Compare = std::less<Key>,
 		class Allocator = std::allocator<Key>
 	>
-	class set {
-		ft::map<Key, void, Compare, Allocator>	c;
+	class set : public map {
 		public:
+			explicit set(const key_compare &comp = key_compare(),
+			const allocator_type &alloc = allocator_type());
+			template <class InputIterator>
+			set(InputIterator first, InputIterator last,
+				const key_compare &comp = key_compare(),
+				const allocator_type &alloc = allocator_type());
+			set(const set &x);
+			~set() {}
 	}
 }
